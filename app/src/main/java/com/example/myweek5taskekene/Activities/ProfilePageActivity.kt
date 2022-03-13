f70b19547c0a1495349ea809e296a165026bc178
Ekene Okoli <developerekene@gmail.com>
2022-03-13 03:56:31 +0100

    1st push

---
+++ app/src/main/java/com/example/myweek5taskekene/Activities/ProfilePageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myweek5taskekene.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myweek5taskekene.R
+
+class ProfilePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_page)
+    }
+}
